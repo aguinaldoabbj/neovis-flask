989207b5dd9e8f5cd620b0e3b2e0f30024a52c86
aguinaldoabbj <aguinaldoabbj@gmail.com>
2019-12-06 15:03:19 -0300

    first working version

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request
 import socket
+import base64
 
 app = Flask(__name__)
 
@@ -12,25 +13,21 @@ def index():
     except:
         return render_template('error.html')
     
-@app.route("/cypher/<cypher>")
-def init_cypher(cypher):
+@app.route("/form")
+def form():
     try:
-        return render_template('neo4j_vis.html', query=cypher)
+        return render_template('neo4j_vis_form.html')
     except:
         return render_template('error.html')
-    
-@app.route("/config/<config>")
-def config_vis(config):
-    try:
-        return render_template('neo4j_vis.html', query=config)
-    except:
-        return render_template('error.html')
-    
+           
 @app.route("/visconfig", methods=['GET'])
 def config_json():
-    query = request.args.get('json', type = str)
+    json = request.args.get('json', type = str)
+    print(json)
+    encodedJSON = str(base64.b64encode(json.encode("utf-8")),"utf-8")
+    print(encodedJSON)
     try:
-        return render_template('neo4j_vis.html', query=query)
+        return render_template('neo4j_vis.html', query=encodedJSON)
     except:
         return render_template('error.html')
         
